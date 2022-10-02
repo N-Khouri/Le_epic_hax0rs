37dc51ba26a712e9b937a15969d28f23ee0142e7
Joey <77253943+jpobzy@users.noreply.github.com>
2022-10-02 19:27:06 -0400

    added functionality to go to contact_info and about html files, fixed css file directory

--- test.py
+++ test.py
@@ -1,28 +1,41 @@
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 
-@app.route("/")
+@app.route("/",methods = ['POST', 'GET'])
 def index():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
-    # return render_template('about.html')
+    # return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    return render_template('login.html')
 
 
-@app.route("/play%20game")
-def playGame():
-    return render_template('play_Game.html')
+@app.route('/about/', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
+@app.route('/contact/')
+def contact_info():
+    return render_template('contact_info.html')
 
 
-@app.route("/leaderboard")
-def leaderboard():
-    return render_template('leaderboard.html')
 
 
-@app.route("/contact%20info")
-def contactInfo():
-    return render_template('contact_Info.html')
+
+
+# @app.route("/play%20game")
+# def playGame():
+#     return render_template('play_Game.html')
+#
+#
+# @app.route("/leaderboard")
+# def leaderboard():
+#     return render_template('leaderboard.html')
+#
+#
+# @app.route("/contact%20info")
+# def contactInfo():
+#     return render_template('contact_Info.html')
 
 
 
