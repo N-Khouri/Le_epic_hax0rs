5fee3f70cd3fa3fd75b8a3212fd6edc4bf047f90
Joey <77253943+jpobzy@users.noreply.github.com>
2022-10-02 20:23:16 -0400

    added testing to input and grab username/password being sent

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 import datetime
 from flask import Flask, render_template, request
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 
 
 @app.route("/",methods = ['POST', 'GET'])
 def index():
-    # return render_template('index.html', utc_dt=datetime.datetime.utcnow())
-    return render_template('login.html')
+    if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+        return redirect(url_for('dashboard', name=user, password = password))
+    else:
+        user = request.args.get('name')
+        return render_template('login.html')
+    # return render_template('login.html')
 
 
 @app.route('/about/', methods=['GET', 'POST'])
@@ -19,23 +26,16 @@ def contact_info():
     return render_template('contact_info.html')
 
 
+@app.route('/dashboard/<name>/<password>')
+def dashboard(name, password):
+    output1 = 'welcome %s' % name
+    output2 = 'password is %s' % password
+    return output1 + ", z" + output2
 
 
 
 
-# @app.route("/play%20game")
-# def playGame():
-#     return render_template('play_Game.html')
-#
-#
-# @app.route("/leaderboard")
-# def leaderboard():
-#     return render_template('leaderboard.html')
-#
-#
-# @app.route("/contact%20info")
-# def contactInfo():
-#     return render_template('contact_Info.html')
+
 
 
 
